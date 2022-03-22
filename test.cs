97a8a563f9edb01dc01758755464f69195a9a2b6
DoctorWho406 <lollo.guerrini@gmail.com>
2022-03-22 09:01:20 +0100

    Create CS for test DotNet-Format

---
+++ test.cs
@@ -0,0 +1,19 @@
+public class Test 
+{
+    public float PUBLICVARIABLE;
+    public float anotherPublicVariable;
+    public float CorrectPublicVariable;
+
+    private float PRIVATEVARIABLE;
+    private float AnotherPrivateVariable;
+    private float correctPrivateVariable;
+
+    public void ExampleMetod() 
+    {
+
+    }
+
+    public void CorrectMetod() {
+
+    }
+}
